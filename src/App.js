7030381bb55471b5f2081e0a966c78386e1b43b9
Thor Denson <tjdenson@eckerd.edu>
2018-07-17 13:39:35 -0400

    added button

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+
+    <div className="container">
+      <button>+</button>
+
+    </div>
+  );
+};
+/* <div>
+  <button>+</button>
+  <div>
+    <div>
+      3
+    </div>
+    <div>
+      1
+    </div>
+    <div>
+      1
+    </div>
+  </div>
+</div> */
+
+export default App;
