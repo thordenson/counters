3cec43cc0bea0df0399ca9b56c415e75222d57e2
Thor Denson <tjdenson@eckerd.edu>
2018-07-18 09:45:05 -0400

    adds reactive React.Component

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
+//Rule #1 : extend Reach.Component
+class Counter extends React.Component {
+
+  //Rule #4: accept props in the constructor method
+  //in Python, this is __init__
+  constructor(props){
+    super(props);
+  }
+
+ // Rule #2: must have render method
+ render() {
+//Rule #3: must return some JSX
+//or a call to React.createElement
+return (
+  <div className='counter'>
+    {this.props.initialValue}
+  </div>
+  );
+ }
+}
+
 // const Counter = (props) => {
 //   console.log(props);
 //   return (
@@ -10,12 +31,12 @@ import React from 'react';
 // };
 //shorthand version (object destructuring):
 
-const Counter = ({finalValue, initialValue}) => {
-  return (
-    <div className='counter'>
-        {initialValue}
-    </div>
-  );
-};
+// const Counter = ({finalValue, initialValue}) => {
+//   return (
+//     <div className='counter'>
+//         {initialValue}
+//     </div>
+//   );
+// };
 
 export default Counter;
